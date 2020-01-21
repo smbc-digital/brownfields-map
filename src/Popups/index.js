ec96cf614a731fa601e62e97907509fc6d68b86a
Priya Ganger <priya.ganger@stockport.gov.uk>
2020-01-21 15:27:24 +0000

    style(Popups): removed hr

--- src/Popups/index.js
+++ src/Popups/index.js
@@ -1,8 +1,10 @@
 const BrownfieldPopup = (feature, layer) => {
   layer.bindPopup(
-    `<div class="item"><i class="tag fa fa-tag"></i><p class="title">Reference: </p><p class="info">${feature.properties.sitereference}</p></div><hr/>
+    `<div class="item"><i class="tag fa fa-tag"></i><p class="title">Reference: </p><p class="info">${feature.properties.sitereference}</p></div>
     <div class="item"><i class="tag fa fa-map-marker"></i><p class="title">Address: </p><p class="info">${feature.properties.sitenameaddress.replace(/\"/g, '')}</p></div>
-    <div class="item"><i class="tag fa fa-users"></i><p class="title">Net Dwellings: </p><p class="info">${feature.properties.hectares}</p></div>
+    <div class="item"><i class="tag fa fa-home"></i><p class="title">Net Dwellings: </p><p class="info">${feature.properties.netdwellingsrangeto}</p></div>
+
+    
      `
   )
 }
