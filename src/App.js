c8c7364a8905f5efc4e3cadf17a88874a0b2f65a
Colin Lees <colin.lees@stockport.gov.uk>
2020-02-10 15:00:26 +0000

    feat(Template) amend template for application

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import React, { useState, useCallback, useRef, useEffect } from 'react'
+import React, { useRef, useEffect, useState } from 'react'
 import Leaflet from 'leaflet'
 import { fetchWithTimeout } from './Helpers'
 import Config from './Configuration.js'
-import { AddLayerControlsLayers, AddLayerControlsOverlays } from './Controls'
 import { os_open } from './Tiles'
+import { AddLayerControlsLayers, AddLayerControlsOverlays } from './Controls'
+import leafletPip from '@mapbox/leaflet-pip'
 
 function App() {
-  const  mapRef = useRef()
-  const [layers, setLayers] = useState([])
+  const mapRef = useRef()
+
   const StaticLayerGroup = {}
   const WMSLayerGroup = {}
   const DynamicLayerGroup = Config.DynamicData.reduce((accumulator, currentValue) => {
@@ -18,19 +19,19 @@ function App() {
   useEffect(() => {
     mapRef.current = Leaflet.map('map', {
       preferCanvas: true,
-      minZoom: 11,
+      minZoom: 12,
       fullscreenControl: Config.Map.FullscreenControl || false,
       layers: [
         os_open
       ]
     }).setView(Config.Map.StartingLatLng || [53.413519, -2.085143], Config.Map.StartingZoom || 12)
-    window.LeafletMap = mapRef.current
-    AddStaticLayers()
-    AddDynamicLayers()
-    AddLayerControls()
+
+    setStaticLayers()
+    setDynamicLayers()
+    setLayerControls()
   }, [])
 
-  const AddLayerControls = () => {
+  const setLayerControls = () => {
     const controlLayers = AddLayerControlsLayers(Config.Map)
     const overlays = AddLayerControlsOverlays(Config, DynamicLayerGroup, WMSLayerGroup, mapRef.current)
 
@@ -41,97 +42,88 @@ function App() {
     }
   }
 
-  const AddDynamicLayers = async () => {
+  const setDynamicLayers = async () => {
+    const layers = {}
     await Promise.all(Config.DynamicData.map(async (layer) => {
       if (layer.url.endsWith('wms?')) {
         WMSLayerGroup[layer.key] = layer
       } else {
         const url = layer.url.replace('{0}', mapRef.current.getBounds().toBBoxString())
-        await fetchDynamicData(layer.key, url, layer.layerOptions)
+        const newLayer = await fetchData(url, layer.layerOptions)
+        layers[layer.key] = newLayer
       }
     }))
 
-    updateLayers()
-  }
-
-  const AddStaticLayers = async () => {
-    await Promise.all(Config.StaticData.map(async (layer) => {
-      await fetchStaticData(layer.key, layer.url, layer.layerOptions)
-    }))
-
-    Config.StaticData.map((layer) => {
-      if (mapRef.current.getZoom() > layers[layer.key].options.maxZoom) {
-        StaticLayerGroup[layer.key] = new Leaflet.FeatureGroup()
-        StaticLayerGroup[layer.key].addLayer(layers[layer.key]).addTo(mapRef.current)
-      }
-    })
-  }
-
-  const updateLayers = () => {
     Object.keys(DynamicLayerGroup).map((layer) => {
       DynamicLayerGroup[layer].clearLayers()
-      if (layers[layer] !== undefined && layers[layer].displayMarkers) {
+      if (layers[layer] !== undefined && layers[layer] !== null) {
         DynamicLayerGroup[layer].addLayer(layers[layer])
       }
     })
+  }
 
-    Object.keys(StaticLayerGroup).map((layer) => {
-      StaticLayerGroup[layer].clearLayers()
-    })
+  const setStaticLayers = async () => {
+    const layers = {}
+    await Promise.all(Config.StaticData.map(async (layer) => {
+      const newLayer = await fetchData(layer.url, layer.layerOptions)
+      layers[layer.key] = newLayer
+    }))
 
     Config.StaticData.map((layer) => {
-      if (layers[layer.key] && mapRef.current.getZoom() > layers[layer.key].options.maxZoom) {
+      if (layers[layer.key] !== null) {
         StaticLayerGroup[layer.key] = new Leaflet.FeatureGroup()
         StaticLayerGroup[layer.key].addLayer(layers[layer.key]).addTo(mapRef.current)
       }
     })
   }
 
-  const handleMapMove = useCallback(async () => {
-    await AddDynamicLayers()
+  useEffect(() => {
+    mapRef.current.addEventListener('moveend', setDynamicLayers)
+
+    return () =>  mapRef.current.removeEventListener('moveend', setDynamicLayers)
   }, [])
 
+  const [onClickLatLng, setOnClickLatLng] = useState()
   useEffect(() => {
-    mapRef.current.addEventListener('moveend', handleMapMove)
-    return () => {
-      mapRef.current.removeEventListener('moveend', handleMapMove)
+    if(!onClickLatLng) return
+
+    const polygonsFoundInMap = leafletPip.pointInLayer(onClickLatLng, mapRef.current)
+
+    const layerContentInMap = polygonsFoundInMap
+      .filter(_ => _.feature && _._popup && _._popup._content)
+      .reduce((acc, curr, index, src) => {
+        return `${acc} ${curr._popup._content} ${index != src.length - 1 ? '<hr/>' : ''}`
+      }, '')
+
+    /** opens new popup with new content and binds to map, this is instead of using 
+     * mapRef.current._popup.setConent as the popup is bound to the layer and not 
+     * the map and will therefore close when you move the map */
+    if(layerContentInMap){
+      Leaflet.popup()
+        .setLatLng(onClickLatLng)
+        .setContent(layerContentInMap)
+        .openOn(mapRef.current)
+    }else{
+      Leaflet.popup()
+        .setLatLng(onClickLatLng)
+        .setContent(mapRef.current._popup._content)
+        .openOn(mapRef.current)
     }
-  }, [handleMapMove])
-
-  const handleOpenPopupEvent = ({ content, latlng }) => {
-    Leaflet.popup()
-      .setLatLng(latlng)
-      .setContent(content)
-      .openOn(mapRef.current)
-  }
-
+  }, [onClickLatLng])
+  const onPopupOpenHandler = event =>  setOnClickLatLng(event.popup._latlng)
   useEffect(() => {
-    mapRef.current.addEventListener('openPopup', handleOpenPopupEvent)
-    return () => {
-      mapRef.current.removeEventListener('openPopup', handleOpenPopupEvent)
-    }
+    mapRef.current.addEventListener('popupopen', onPopupOpenHandler)
+
+    return () => mapRef.current.removeEventListener('popupopen', onPopupOpenHandler)
   }, [])
 
-  const fetchDynamicData = async (key, url, layerOptions) => {
+  const fetchData = async (url, layerOptions) => {
     if (mapRef.current.getZoom() > layerOptions.maxZoom) {
       const response = await fetchWithTimeout(url)
       const body = await response.json()
-      const layer = Leaflet.geoJson(body, layerOptions)
-      layer.displayMarkers = true
-      setLayers([...layers, layers[key] = layer])
+      return Leaflet.geoJson(body, layerOptions)
     }
-    else {
-      const layer = {}
-      layer.displayMarkers = false
-      setLayers([...layers, layers[key] = layer])
-    }
-  }
-
-  const fetchStaticData = async (key, url, layerOptions) => {
-    const response = await fetchWithTimeout(url)
-    const body = await response.json()
-    const layer = Leaflet.geoJson(body, layerOptions)
-    setLayers([...layers, layers[key] = layer])
+    return null
   }
 
   return (
