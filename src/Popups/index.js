54c3b188889ea11fca603d81353c1cf7b1e1d863
Dan Kinder <d.kinder02@gmail.com>
2020-12-10 12:19:05 +0000

    Popup changes

--- src/Popups/index.js
+++ src/Popups/index.js
@@ -1,20 +1,28 @@
   
 const brownfield2019Popup = (feature, layer) => {
-  layer.bindPopup(
-    `<div class="item"><i class="tag fa fa-tag"></i><p class="title">Reference: </p><p class="info">${feature.properties.sitereference}</p></div>
-    <div class="item"><i class="tag fa fa-map-marker"></i><p class="title">Address: </p><p class="info">${feature.properties.sitenameaddress.replace(/"/g, '')}</p></div>
-    <div class="item"><i class="tag fa fa-home"></i><p class="title">Net Dwellings: </p><p class="info">${feature.properties.netdwellingsrangeto}</p></div>
-     `
-  )
+  
+  const content = `<div class="item"><p class="title">Brownfield Land Register 2019</p>
+    <p></p>
+    <p class="item"><i class="tag fa fa-tag"></i><p class="title">Reference: </p><p class="info">${feature.properties.sitereference}</p>
+    <p class="item"><i class="tag fa fa-map-marker"></i><p class="title">Address: </p><p class="info">${feature.properties.sitenameaddress.replace(/"/g, '')}</p>
+    <p class="item"><i class="tag fa fa-home"></i><p class="title">Net Dwellings: </p><p class="info">${feature.properties.netdwellingsrangeto}</p>
+    
+    </div>`
+
+  layer.bindPopup(content)
 }
 
 const brownfield2020Popup = (feature, layer) => {
-  layer.bindPopup(
-    `<div class="item"><i class="tag fa fa-tag"></i><p class="title">Reference: </p><p class="info">${feature.properties.sitereference}</p></div>
-    <div class="item"><i class="tag fa fa-map-marker"></i><p class="title">Address: </p><p class="info">${feature.properties.sitenameaddress.replace(/"/g, '')}</p></div>
-    <div class="item"><i class="tag fa fa-home"></i><p class="title">Net Dwellings: </p><p class="info">${feature.properties.netdwellingsrangefrom}</p></div>
-     `
-  )
+  
+  const content = `<div class="item"><p class="title">Brownfield Land Register 2020</p>
+    <p></p>
+    <p class="item"><i class="tag fa fa-tag"></i><p class="title">Reference: </p><p class="info">${feature.properties.sitereference}</p>
+    <p class="item"><i class="tag fa fa-map-marker"></i><p class="title">Address: </p><p class="info">${feature.properties.sitenameaddress.replace(/"/g, '')}</p>
+    <p class="item"><i class="tag fa fa-home"></i><p class="title">Net Dwellings: </p><p class="info">${feature.properties.netdwellingsrangefrom}</p>
+    
+    </div>`
+
+  layer.bindPopup(content)
 }
 
 
